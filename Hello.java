7181b68d8c2a4d49bd01c2c04bde20dae9078f2c
yangfan <1370017349@qq.com>
2019-07-19 22:24:43 +0800

    第一次修改Hello.java

--- Hello.java
+++ Hello.java
@@ -7,19 +7,5 @@ public class Hello{
 		System.out.println("Hello World!");
 	}
 	
-	// 测试方法
-	public void test(){
-		serr("测试测试!!!");
-		System.out.println("测试方法!");
-		System.out.println("master!!!!!");
-	}
-	// 测试方法
-	public void test(){
-		serr("测试测试!!!");
-		System.out.println("测试方法!");
-		System.out.println("master!!!!!");
-		System.out.println("branch2!!!!");
-		System.out.println("local!!!!");
-		System.out.println("remote!!!!");
-	}
+	
 }
