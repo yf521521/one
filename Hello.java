d73f0f028f1c62185ccc9367d8425dcd9d4b6028
yangfan <1370017349@qq.com>
2019-07-19 22:19:16 +0800

    我的git测试第一次上传

---
+++ Hello.java
@@ -0,0 +1,25 @@
+public class Hello{
+	public static void main(String[] args){	
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args){	
+		System.out.println("Hello World!");
+	}
+	
+	// 测试方法
+	public void test(){
+		serr("测试测试!!!");
+		System.out.println("测试方法!");
+		System.out.println("master!!!!!");
+	}
+	// 测试方法
+	public void test(){
+		serr("测试测试!!!");
+		System.out.println("测试方法!");
+		System.out.println("master!!!!!");
+		System.out.println("branch2!!!!");
+		System.out.println("local!!!!");
+		System.out.println("remote!!!!");
+	}
+}
